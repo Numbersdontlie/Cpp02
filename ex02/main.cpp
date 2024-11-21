eb33771c2707f10d3fb9ac898cdb69e94e7437fc
Numbersdontlie <luifer92@gmail.com>
2024-11-21 23:23:26 +0100

    progress of solution to exercise 2

---
+++ ex02/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luifer <luifer@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 23:16:28 by luifer            #+#    #+#             */
+/*   Updated: 2024/11/21 23:21:42 by luifer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(void){
+    Fixed a;
+    Fixed const b(Fixed(5.05f) * Fixed(2));
+
+    std::cout << BLUE << a << RESET << std::endl;
+    std::cout << BLUE << ++a << RESET << std::endl;
+    std::cout << BLUE << a << RESET << std::endl;
+    std::cout << BLUE << a++ << RESET << std::endl;
+    std::cout << BLUE << a << RESET << std::endl;
+    std::cout << BLUE << --a << RESET << std::endl;
+    std::cout << BLUE << a << RESET << std::endl;
+    std::cout << BLUE << a-- << RESET << std::endl;
+    std::cout << BLUE << a << RESET << std::endl;
+    
+
+    std::cout << b << std::endl;
+
+    std::cout << Fixed::max(a, b) << std::endl;
+    return(0);
+}
